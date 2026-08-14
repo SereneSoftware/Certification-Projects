920538cd945a61d1b17e867f581afe02474f8ee8
SereneSoftware <aanyamassey05@gmail.com>
2026-08-14 19:50:24 +0530

    Add JavaScript trivia bot exercise

---
+++ JavaScript Trivia Bot/trivia-bot.js
@@ -0,0 +1,27 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName;
+let botLocation;
+let favoriteLanguage;
+botName = "Brian";
+botLocation = "earth";
+favoriteLanguage = "Python";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact =
+  favoriteLanguage +
+  " is dynamically typed language and has features such as Object Oriented Programming!";
+console.log(codingFact);
+codingFact =
+  favoriteLanguage +
+  " is very easy to understand and has very easy to learn syntax.";
+console.log(codingFact);
+codingFact =
+  favoriteLanguage + " is used in Machine Learning, Robotics and Data Science.";
+console.log(codingFact);
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    ".",
+);

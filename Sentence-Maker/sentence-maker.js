0c15e5157b3c3ca9ff0380c8e2d7a4eab04c2408
SereneSoftware <aanyamassey05@gmail.com>
2026-08-17 12:14:43 +0530

    Add JavaScript sentence maker exercise

---
+++ Sentence-Maker/sentence-maker.js
@@ -0,0 +1,48 @@
+let adjective = "big";
+let noun = "dragon";
+let noun2 = "humans";
+let place = "mountain";
+let adjective2 = "big";
+let verb = "angry";
+let firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " " +
+  "who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("First story: " + firstStory);
+adjective = "happy";
+noun = "pegasus";
+noun2 = "apples";
+place = "forest";
+adjective2 = "enormous";
+verb = "sleeping";
+let secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " " +
+  "who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("Second story: " + secondStory);
